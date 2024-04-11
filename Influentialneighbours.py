dba8dccf8b6292bcf26ac07c97726eec9292e796
Blo-field <liamjack99@hotmail.co.uk>
2024-04-11 12:05:09 +0100

    'influentialneighbours'

---
+++ Influentialneighbours.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+#epsilon = [3,5]
+
+#Cmin = [0.995,0.99,0.95,0.5]
+
+data_file = "C:/Users/TheBuild/OneDrive - University of Bristol/Documents/University/Year 2/MDM2/Project3/FishData/exp02H20141128_16h06updated.csv"
+start_row = 150
+df = pd.read_csv(data_file, skiprows=start_row,nrows=500, names=['X1','Y1','H1','X2','Y2','H2','VX1','VY1','VX2','VY2','UVX1','UVY1','UVX2','UVY2'])
+
+invalid_strings = ['#DIV/0!', 'NA', '#VALUE!']
+for col in df.columns:
+    for invalid_str in invalid_strings:
+        df = df[df[col] != invalid_str]
+
+# Convert remaining values to floats
+df = df.apply(pd.to_numeric, errors='ignore')
+
+# Drop rows with NaN values
+df = df.dropna()
+
+fish1_uvx = df["UVX1"].to_numpy()
+fish1_uvy = df["UVY1"].to_numpy()
+fish2_uvx = df["UVX2"].to_numpy()
+fish2_uvy = df["UVY2"].to_numpy()
+
+fish1_velocities = np.stack((fish1_uvx, fish1_uvy), axis=1)  
+fish2_velocities = np.stack((fish2_uvx, fish2_uvy), axis=1)  
+
+w = 4  
+
+tau_0 = 50
+max_time_delay = 100  # Adjust this based on your analysis
+time_delays = range(tau_0, max_time_delay + 1)  # Create your R_k
+
+Gamma_values = []
+for t_k in range(len(fish1_velocities) - w):  
+    max_correlation = 0.95  # Initialize to find the maximum
+    for tau_r in range(min(t_k, max_time_delay)):
+        # Calculate C_ij(t_k, tau_r, w) 
+        start_index = t_k 
+        end_index = t_k + w
+        current_velocities = fish1_velocities[start_index:end_index]
+        shifted_velocities = fish2_velocities[start_index - tau_r:end_index - tau_r]
+
+        dot_products = np.einsum('ij,ij->i', current_velocities, shifted_velocities)
+        correlation = np.average(dot_products)  # Average correlation within window
+
+        max_correlation = max(max_correlation, correlation)
+        #print(max_correlation)
+
+    Gamma_values.append(max_correlation)
+
+
+for t_k, gamma in enumerate(Gamma_values):
+    if gamma > 0.95:  # Adjust the threshold as needed
+        print(f"Fish 2 is likely influencing Fish 1 at time t_k = {t_k}") 
+    else: 
+        print(f"Fish 1 is likely influencing Fish 2 at time t_k = {t_k}") 
+
+
+
