93cc68d2bfb40a5094a86c3c80066c49b12e875d
Blo-field <liamjack99@hotmail.co.uk>
2024-04-18 14:23:52 +0100

    turned off the t_k prints

--- FunctionInfluential.py
+++ FunctionInfluential.py
@@ -60,19 +60,19 @@ def influential(file,start_frame,num_frames):
         
 
 
-    for t_k, gamma in enumerate(Gamma_values):
-        if gamma > 0.95:  # Adjust the threshold as needed
+    #for t_k, gamma in enumerate(Gamma_values):
+    #    if gamma > 0.95:  # Adjust the threshold as needed
             print(f"Fish 2 is likely influencing Fish 1 at time t_k = {t_k}") 
-        else: 
+    #    else: 
             print(f"Fish 1 is likely influencing Fish 2 at time t_k = {t_k}") 
-    return Gamma_values[1:] #skips the first value as this is just whatever is set as correlation above so total size will be + -1 + -w*n eg 1000 rows will give you 979 or 800 rows will give you 783
+    return Gamma_values[1:] #skips the first value as this is just whatever is set as correlation above so total size will be + -1 + -w*n eg 1000 rows will give you 979 or 800 rows will give you 779 depends on how many loops are ran
 
 
  
 #put file to test function
-data_file = "C:\\Users\\TheBuild\\OneDrive - University of Bristol\\Documents\\University\\Year 2\\MDM2\\Project3\\Python Code\\MDM2-3\\FishData\\exp02H20141128_16h06updated.csv"
+#data_file = "C:\\Users\\TheBuild\\OneDrive - University of Bristol\\Documents\\University\\Year 2\\MDM2\\Project3\\Python Code\\MDM2-3\\FishData\\exp02H20141128_16h06updated.csv"
 
-gamma = influential(data_file,1,800)
-number_of_elements = len(gamma)
-print(f"The size of the gamma list is: {number_of_elements}")
-print(gamma)
+#gamma = influential(data_file,1,800)
+##number_of_elements = len(gamma)
+##print(f"The size of the gamma list is: {number_of_elements}")
+#print(gamma)
